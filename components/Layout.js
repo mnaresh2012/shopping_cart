3ed38dd8deb8c803f246796fe5cdb46bc2f7108d
naresh kumar <mnaresh2010@aol.com>
2021-08-08 18:46:35 +0530

    Delete components directory

--- components/Layout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { withRedux } from "../../lib/with-redux-store";
-
-function Layout({ children }) {
-  return <div>{children}</div>;
-}
-
-export default withRedux(Layout);
